691d708fc8d4b7f769806acf515becc0361e070d
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-21 16:53:26 +0600

    added

--- 04.java
+++ 04.java
@@ -6,3 +6,15 @@ public class Main {
 
         System.out.print("Enter Hour of the Day: ");
         int a = scanner.nextInt();
+
+        if (a < 10) {
+            System.out.println("Good morning");
+        } else if (a < 20) {
+            System.out.println("Good day");
+        } else {
+            System.out.println("Good evening");
+        }
+
+        scanner.close();
+    }
+}
