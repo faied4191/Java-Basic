b92a4eda81e917912225b9ee008116e82ee661e6
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-21 16:52:50 +0600

    added

--- 04.java
+++ 04.java
@@ -3,3 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Hour of the Day: ");
+        int a = scanner.nextInt();
