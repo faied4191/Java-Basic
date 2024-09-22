03b4cf0e99f58c4dbe34949a6f5bb0190ef8b1be
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-23 05:29:40 +0600

    added

--- 06.java
+++ 06.java
@@ -3,3 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Day Number of the Week: ");
+        int a = scanner.nextInt();
