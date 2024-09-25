06dfb4e8a4aaa14862660a9293216eda39e31e05
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-25 09:11:17 +0600

    added

--- 10.java
+++ 10.java
@@ -5,3 +5,8 @@ public class Main {
     public static float isSum(float x, float y) {
         return x + y;
     }
+
+    // Method to find the maximum of two numbers
+    public static float isMax(float x, float y) {
+        return (x > y) ? x : y;
+    }
