05ac243ac08325f8b607074b5796c72c1295e1e5
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-01 02:15:44 +0600

    added

--- 14.java
+++ 14.java
@@ -5,3 +5,10 @@ public class Main {
         String title;
         String author;
         int year;
+
+        // Constructor
+        Book(String title, String author, int year) {
+            this.title = title;
+            this.author = author;
+            this.year = year;
+        }
