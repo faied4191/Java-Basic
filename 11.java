cb640089f2b91871962ab2e9402334d41a8855a6
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-30 17:58:59 +0600

    added

--- 11.java
+++ 11.java
@@ -1,3 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         String[] arr1 = {"Volvo", "BMW", "Ford", "Mazda"};
+        arr1[0] = "Opel";
+
+        System.out.println("First Element Name: " + arr1[0]);
+        System.out.println("First Element Length: " + arr1[0].length());
+        System.out.println("Array Size: " + arr1.length);
+    }
+}
