a42b446dc30e4d3db78debd1505adab30f2eca43
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-01 02:21:21 +0600

    added

---
+++ 16.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    // Define the Animal class
+    static class Animal {
+        // Method for eating
+        void eat() {
+            System.out.println("Eat");
+        }
