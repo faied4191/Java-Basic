2d60dc27dd0fb11f95d06d621933b3e6d6b5e0d4
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-30 18:04:15 +0600

    added

--- 13.java
+++ 13.java
@@ -3,3 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int a = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character left by nextInt()
