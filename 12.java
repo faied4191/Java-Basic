30e1c67456a865c19545a3a5dc377a51f0fa3553
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-09-30 18:02:17 +0600

    added

--- 12.java
+++ 12.java
@@ -9,3 +9,16 @@ public class Main {
         scanner.nextLine(); // Consume the newline character left by nextInt()
 
         String[] arr1 = new String[a];
+
+        for (int i = 0; i < a; i++) {
+            System.out.print("Enter element name " + (i + 1) + ": ");
+            arr1[i] = scanner.nextLine();
+        }
+
+        System.out.println("First Element Name: " + arr1[0]);
+        System.out.println("First Element Length: " + arr1[0].length());
+        System.out.println("Array Size: " + arr1.length);
+
+        scanner.close();
+    }
+}
