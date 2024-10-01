a1425074cb747b9d256dae3b4b6a8dcfdad8653f
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-02 02:02:59 +0600

    added

---
+++ 17.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    // Define the Calculator class
+    static class Calculator {
+
+        // Method to add two integers
+        void add(int a, int b) {
+            System.out.println("The Sum is: " + (a + b));
+        }
