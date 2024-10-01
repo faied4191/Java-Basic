f8930f09e0a83b28249fbb508a54bb53301ab967
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-02 02:04:55 +0600

    added

--- 17.java
+++ 17.java
@@ -18,3 +18,14 @@ public class Main {
             System.out.println("The Sum is: " + (a + b + c));
         }
     }
+
+    public static void main(String[] args) {
+        // Create a Calculator object
+        Calculator ob = new Calculator();
+
+        // Call overloaded methods
+        ob.add(5, 3);         // Calls the method with two integers
+        ob.add(2.5, 1.8);     // Calls the method with two doubles
+        ob.add(1, 2, 3);      // Calls the method with three integers
+    }
+}
